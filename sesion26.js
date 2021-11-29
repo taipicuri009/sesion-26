563d78d2a6330152bdc00f96547956eb54331703
taipicuri009 <priscilataipicuri009@gmail.com>
2021-11-29 14:17:53 -0500

    subiendo

---
+++ sesion26.js
@@ -0,0 +1,25 @@
+/*function numero (a){
+    return a;
+    }
+    
+    console.log (numero(5));
+
+    function suma (a,b){
+        suma = 11 + 9
+        return suma ;
+        }
+        console.log("la respuesta es :"+suma (11+9))
+
+        function suma(a,b,c)
+    suma = a + b +c
+    return suma;
+
+function promedio(a,b,c,d,e,f,g){
+    promedio= (a + b + c + d + e + f + g)/8
+        return promedio;
+}
+console.log("la suma de los numeros es:" + suma(3,6,4))
+console.log("el promedio de es:" + promedio(3,5,8,2,3,1,2))*/
+
+let nombre = h => h;
+alert(nombre("priscila"));
